b84a6e563e527d1d4a82470fa9a1a8e75c1678f8
ryleybridges <rye.brid@gmail.com>
2019-11-10 16:09:35 +1300

    Made potential changes to the home page layout, commented them out as I consider whether or not I used them or not

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@ const workCardInfo = [
     {
         id: 1,
         title: `BookIt Accommodation`,
-        description: `Application worked on for class summative. Involved creating an application that would present relevant accommodation to a users input.`,
-        longDescription: `Brief for project was to create a one page site for either a transport application or an accommodation application. Pre-coding involved the UX Process (research, target audience, personas, concept sketches, wireframes, user testing). Full documentation explains the process of how it all came together to the final design.`,
+        description: `Application worked on for class summative. Involved creating an application that would present relevant accommodation to a users input. All put together one index page.`,
+        longDescription: `Brief for project was to create a one page site for either a transport application or an accommodation application for Tourism New Zealand. Pre-coding involved the UX Process (research, target audience, personas, concept sketches, wireframes, user testing). Full documentation explains the process of how it all came together to the final design. Using data in the brief and finding information on local accommodation in the area, I was able to put together an application that filters out relevant accommodation judging by the users input of how many nights, arrival date, how many people and where you are wanting to go.`,
         githublink: `https://github.com/ryleybridges/accommodationApp`,
         googledrivelink: `https://drive.google.com/file/d/1CgHYu3lOLPXdYNna1upWybdPzOGGmf7D/view?usp=sharing`,
         image_one: `images/accommodationapp-sketches.jpg`,
-        image_one_caption: `Initial sketches`,
+        image_one_caption: `Initial sketches - helped establish the initial layout for the different parts of the application.`,
         image_two: `images/accommodationapp-sketches2.jpg`,
-        image_two_caption: `Further development on sketches`,
+        image_two_caption: `Further development on sketches - used layout established in first sketches and carried it through in these sketches`,
         image_three: `images/accommodationapp-prototype.png`,
-        image_three_caption: `Development of sketch into prototype on UXPin`,
+        image_three_caption: `Development of sketch into prototype on UXPin upon finalising layout`,
         image_four: `images/accommodationapp.png`,
         image_four_caption: `Final accommodation app homepage`
     },
@@ -19,30 +19,34 @@ const workCardInfo = [
         id: 2,
         title: `Geek Culture Store`,
         description: `Speciality customer to customer store created for a group project. Has a front end and a back end done separately. Has ability to register account, log in and add, edit and delete items.`,
-        longDescription: `Brief involved working as a group to create a customer to customer speciality store. Our group of three chose pop culture as the theme as we all have an interest in it. Followed the UX Process and worked agile in three different sprints.`,
+        longDescription: `Brief involved working as a group to create a customer to customer speciality store. Our group of three consisted of myself, Brayden Gray and Simon Watson. Together, we chose pop culture merchandise as the theme as we all have an interest in it. Followed the UX Process and worked agile in three different sprints.`,
         githublink: `https://github.com/CyborgSemon/summative-3-front`,
         googledrivelink: `https://drive.google.com/file/d/1zMQH6ugOZhmdXgBoRqLOxYjDfiEp3blX/view?usp=sharing`,
         image_one: `images/geekculture-sketches.jpg`,
-        image_one_caption: `Beginning of the sketches`,
+        image_one_caption: `Beginning of the sketches for layout options`,
         image_two: `images/geekculture-sketches2.jpg`,
-        image_two_caption: `Further sketch work`,
+        image_two_caption: `Further sketch work once chatting with group members`,
         image_three: `images/geekculture-prototype.png`,
-        image_three_caption: `Prototype created through UXPin`,
+        image_three_caption: `Prototype created through UXPin with ideas from other group members`,
         image_four: `images/geekculturestore.png`,
         image_four_caption: `Final website layout`
     },
-    // {
-    //     id: 3,
-    //     title: `Tawa Taekwondo Website`,
-    //     description: `To give the Tawa Taekwondo Club an online prescence, I created a website for them as a personal project. A simplistic layout with a contact section for users to get in contact with the club.`,
-    //     longDescription: `As a member of the Tawa Taekwondo Club, I wanted to give the club an online prescence as there is hardly any information out there on the club and no way to get properly in contact with them. The main intention of this site is to inform potential members on the club's history, what Taekwondo is and a contact form to send a message via email to the club.`,
-    //     githublink: `https://github.com/ryleybridges/tawatkdwebsite`,
-    //     googledrivelink: ``,
-    //     // image_one: ,
-    //     // image_two: ,
-    //     // image_three: ,
-    //     image_four: `images/tawatkd.png`
-    // }
+    {
+        id: 3,
+        title: `Tawa Taekwondo Website`,
+        description: `To give the Tawa Taekwondo Club an online prescence, I created a website for them as a personal project. A simplistic layout with a contact section for users to get in contact with the club.`,
+        longDescription: `As a member of the Tawa Taekwondo Club, I wanted to give the club an online prescence as there is hardly any information out there on the club and no way to get properly in contact with them. The main intention of this site is to inform potential members on the club's history, what Taekwondo is and a contact form to send a message via email to a club email address.`,
+        githublink: `https://github.com/ryleybridges/tawatkdwebsite`,
+        googledrivelink: ``,
+        image_one: ``,
+        image_one_caption: ``,
+        image_two: ``,
+        image_two_caption: ``,
+        image_three: ``,
+        image_three_caption: ``,
+        image_four: `images/tawatkd.png`,
+        image_four_caption: `Final website layout`
+    }
 ];
 
 const otherProjects = [
@@ -57,7 +61,7 @@ const otherProjects = [
 function cardThumbnails(){
     let workCardDisplay = ``;
     for (var i = 0; i < workCardInfo.length; i++) {
-        workCardDisplay += `<div class="card ml-2 h-100 col-4" style="width: 30rem;">
+        workCardDisplay += `<div class="card ml-2 h-100 col-md-4" style="width: 30rem;">
                                 <img src="${workCardInfo[i].image_four}" class="card-img-top" alt="${workCardInfo[i].title}">
                                     <div class="card-body">
                                         <h5 class="card-title">${workCardInfo[i].title}</h5>
